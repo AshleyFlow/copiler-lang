c01b74edde319aee161a0c8f871ccea2c52e98f7
Flow <highflowey@gmail.com>
2024-05-11 15:39:49 +0330

    less borrowing in cursor

--- src/cursor.rs
+++ src/cursor.rs
@@ -5,38 +5,42 @@ pub struct Cursor<T> {
 
 impl<T> Cursor<T>
 where
-    T: PartialEq<T>,
+    T: PartialEq<T> + Clone,
 {
     pub fn new(items: Vec<T>) -> Self {
         Self { items, pos: 0 }
     }
 
-    pub fn eat(&mut self) {
-        self.pos += 1;
-    }
-
-    pub fn eat_if(&mut self, expected: T) -> bool {
+    pub fn eat(&mut self) -> Option<T> {
         let peeked = self.peek(None);
 
-        if peeked.is_some() && *peeked.unwrap() == expected {
+        if peeked.is_some() {
             self.pos += 1;
 
-            true
+            Some(self.items[self.pos].clone())
         } else {
-            false
+            None
         }
     }
 
-    pub fn current(&self) -> &T {
-        &self.items[self.pos]
+    pub fn eat_if(&mut self, expected: T) -> Option<T> {
+        let peeked = self.peek(None);
+
+        if peeked.is_some() && peeked.unwrap() == expected {
+            self.pos += 1;
+
+            Some(self.items[self.pos].clone())
+        } else {
+            None
+        }
     }
 
-    pub fn peek(&self, offset: Option<usize>) -> Option<&T> {
+    pub fn peek(&self, offset: Option<usize>) -> Option<T> {
         let offset = offset.unwrap_or(1);
         let pos = self.pos + offset;
 
         if pos < self.items.len() {
-            Some(&self.items[pos])
+            Some(self.items[pos].clone())
         } else {
             None
         }
