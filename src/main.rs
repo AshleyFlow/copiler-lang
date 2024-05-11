dc11d157d7aaf207b7f0eb95ac07e608096f0b84
Flow <highflowey@gmail.com>
2024-05-11 16:22:45 +0330

    delete parser

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,11 @@
+use lexer::Lexer;
+
+pub mod cursor;
 mod lexer;
 
-use crate::lexer::{Lexer, Token};
-use std::iter::Peekable;
-
-struct Parser<'a> {
-    iter: Peekable<core::slice::Iter<'a, Token>>,
-}
-
-impl<'parser> Parser<'parser> {
-    fn next(&mut self) {
-        let token = self.iter.peek();
-
-        if let Some(_token) = token {
-            todo!()
-        }
-    }
-
-    pub fn load(&mut self) {
-        while self.iter.peek().is_some() {
-            self.next();
-        }
-    }
-
-    pub fn new(tokens: &'parser [Token]) -> Self {
-        let iter = tokens.iter().peekable();
-
-        Self { iter }
-    }
-}
-
 fn main() {
-    let src = "print(10.25) exit(0)";
+    let src = "exit(0)";
 
     let mut lexer = Lexer::new(src);
-    let tokens = lexer.load();
-
-    let mut parser = Parser::new(tokens);
-    parser.load();
-
-    std::process::exit(0);
+    let _tokens = lexer.load();
 }
