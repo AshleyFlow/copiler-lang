e8bd4bf6bcc7da4343e3c994c958052c92c43f29
Flow <highflowey@gmail.com>
2024-05-21 17:13:45 +0330

    move parser to parser.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use cursor::Cursor;
-use lexer::{Lexer, Literal, Token};
+use crate::parser::Parser;
+use lexer::Lexer;
 
 pub mod cursor;
 mod lexer;
+mod parser;
 
 const SRC: &str = r#"
 
@@ -19,87 +20,6 @@ let a = "This is a string"
 let b = 25.5234
 
 "#;
-
-#[derive(Debug)]
-#[allow(dead_code)]
-enum Expression {
-    Variable(String, Literal),
-    Value(Literal),
-    Scope(Vec<Expression>),
-}
-
-struct Parser {
-    cursor: Cursor<Token>,
-}
-
-impl Parser {
-    fn variable(&mut self) -> Expression {
-        let identifier = if let Token::Identifier(identifier) = self.cursor.eat().unwrap() {
-            identifier
-        } else {
-            panic!()
-        };
-
-        self.cursor.eat_iff(|token| {
-            if let Token::Equal = token {
-                true
-            } else {
-                panic!("Expected '=', got '{:?}'", token)
-            }
-        });
-
-        let value = if let Token::Literal(literal) = self.cursor.eat().unwrap() {
-            literal
-        } else {
-            panic!()
-        };
-
-        Expression::Variable(identifier, value)
-    }
-
-    pub fn parse_expression(&mut self) -> Option<Expression> {
-        if let Some(token) = self.cursor.eat() {
-            match token {
-                Token::Literal(literal) => {
-                    let expr = Expression::Value(literal);
-                    Some(expr)
-                }
-                Token::Identifier(identifier) => {
-                    if identifier == "let" {
-                        Some(self.variable())
-                    } else {
-                        panic!("Unexpected identifier '{}'", identifier)
-                    }
-                }
-                Token::ScopeOpen => Some(self.parse_scope()),
-                Token::ScopeClose => None,
-                _ => todo!(),
-            }
-        } else {
-            None
-        }
-    }
-
-    pub fn parse_scope(&mut self) -> Expression {
-        let mut stack: Vec<Expression> = Vec::new();
-
-        while let Some(expr) = self.parse_expression() {
-            stack.push(expr);
-        }
-
-        Expression::Scope(stack)
-    }
-
-    pub fn load(&mut self) -> Expression {
-        self.parse_scope()
-    }
-
-    pub fn new(tokens: &[Token]) -> Self {
-        Self {
-            cursor: Cursor::new(tokens.to_owned()),
-        }
-    }
-}
 
 fn main() {
     let mut lexer = Lexer::new(SRC);
---
+++ src/parser.rs
@@ -0,0 +1,82 @@
+use crate::cursor::Cursor;
+use crate::lexer::{Literal, Token};
+
+#[derive(Debug)]
+pub enum Expression {
+    Variable(String, Literal),
+    Value(Literal),
+    Scope(Vec<Expression>),
+}
+
+pub struct Parser {
+    cursor: Cursor<Token>,
+}
+
+impl Parser {
+    fn variable(&mut self) -> Expression {
+        let identifier = if let Token::Identifier(identifier) = self.cursor.eat().unwrap() {
+            identifier
+        } else {
+            panic!()
+        };
+
+        self.cursor.eat_iff(|token| {
+            if let Token::Equal = token {
+                true
+            } else {
+                panic!("Expected '=', got '{:?}'", token)
+            }
+        });
+
+        let value = if let Token::Literal(literal) = self.cursor.eat().unwrap() {
+            literal
+        } else {
+            panic!()
+        };
+
+        Expression::Variable(identifier, value)
+    }
+
+    pub fn parse_expression(&mut self) -> Option<Expression> {
+        if let Some(token) = self.cursor.eat() {
+            match token {
+                Token::Literal(literal) => {
+                    let expr = Expression::Value(literal);
+                    Some(expr)
+                }
+                Token::Identifier(identifier) => {
+                    if identifier == "let" {
+                        Some(self.variable())
+                    } else {
+                        panic!("Unexpected identifier '{}'", identifier)
+                    }
+                }
+                Token::ScopeOpen => Some(self.parse_scope()),
+                Token::ScopeClose => None,
+                _ => todo!(),
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn parse_scope(&mut self) -> Expression {
+        let mut stack: Vec<Expression> = Vec::new();
+
+        while let Some(expr) = self.parse_expression() {
+            stack.push(expr);
+        }
+
+        Expression::Scope(stack)
+    }
+
+    pub fn load(&mut self) -> Expression {
+        self.parse_scope()
+    }
+
+    pub fn new(tokens: &[Token]) -> Self {
+        Self {
+            cursor: Cursor::new(tokens.to_owned()),
+        }
+    }
+}
